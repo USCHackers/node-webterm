d6b0fd87ce97642ae47e1d956cf8dee6aa0f15ac
Brian Anglin <brianranglin+gh@gmail.com>
2017-09-10 18:35:32 -0700

    Adds download method

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const express = require('express')
       , bodyParser = require('body-parser')
       , csvWriter = require('csv-write-stream')
+      , path = require('path')
       , fs = require('fs');
 
 let app = express()
 
 app.use(express.static('public'))
+
 app.use(bodyParser.json());
 
 app.post('/attendees', (req, res) => {
   var writer = csvWriter();
-  writer.pipe(fs.createWriteStream('.data/output.csv', {flags: 'a'}));
+  writer.pipe(fs.createWriteStream('output.csv', {flags: 'a'}));
   writer.write({name: req.body.name, email: req.body.email, md5: req.body.md5});
   writer.end();
   console.log(req.body);
   res.sendStatus(200);
+});
+
+app.get('/download/:secret', (req, res)  => {
+  if (req.params.secret === process.env.SECRET) {
+    res.sendFile(path.join(__dirname, 'output.csv'));
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 console.log('Visit http://localhost:3000')
